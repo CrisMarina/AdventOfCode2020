a89789b28a4f515b209765b2c5be69e0dc959aea
Cris <cris.marina12@gmail.com>
2020-12-02 22:03:57 +0200

    Day 2 Problem 1

---
+++ src/main/java/Day2/AdventDay2Problem1.java
@@ -0,0 +1,42 @@
+package Day2;
+
+import Utils.AdventUtils;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class AdventDay2Problem1 {
+
+    private static int computeNumberOfRightPasswords() {
+        List<String> values = AdventUtils.readFromFileToList("src/main/resources/AdventDay1Problem2Input");
+        int count = 0;
+        for (String s : values) {
+            // s = 9-10 b: bbktbbbxhfbpb
+            // split = 9-10 b, bbktbbbxhfbpb
+            String[] split = s.split(":");
+            // rule = 9-10 b
+            String rule = split[0];
+            // rules = 9-10, b
+            String[] rules = rule.split(" ");
+            // letter = b
+            String letter = rules[1];
+            // minMax = 9, 10
+            String[] minMax = rules[0].split("-");
+            // minTimes = 9
+            int minTimes = Integer.parseInt(minMax[0]);
+            // maxTimes = 10
+            int maxTimes = Integer.parseInt(minMax[1]);
+
+            //pass = bbktbbbxhfbpb
+            String pass = split[1];
+
+            int letterCount = StringUtils.countMatches(pass, letter);
+
+            if (minTimes <= letterCount && letterCount <= maxTimes) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
