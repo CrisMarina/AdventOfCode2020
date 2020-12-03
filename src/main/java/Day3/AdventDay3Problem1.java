c051bf0049d6f39a756c61ef5f751d96ce97d630
Cris <cris.marina12@gmail.com>
2020-12-03 14:53:57 +0200

    Day 3 Problem 1

---
+++ src/main/java/Day3/AdventDay3Problem1.java
@@ -0,0 +1,35 @@
+package Day3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AdventDay3Problem1 {
+
+    private static int computeNumberOfTrees() {
+        int position = 3;
+        int trees = 0;
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "src/main/resources/AdventDay1Problem3Input"));
+            String line = reader.readLine();
+            while (line != null) {
+                // read next line
+                line = reader.readLine();
+                if (line != null) {
+                    int lineLength = line.length();
+                    char c = line.charAt(position % lineLength);
+                    if ('#' == c) {
+                        trees++;
+                    }
+                    position += 3;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return trees;
+    }
+}
